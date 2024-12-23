b48f9e5c1364f2416d67302d4160a218723bfdca
Amdadul Haque <amdadul1807107@gmail.com>
2024-12-24 00:13:25 +0600

    fix: build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const config = require('./config/config');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,13 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://amdadul1807107:DNJ8evzzuxWKjvV4@cluster0.sjniy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
-
-// Define your routes here
+// Define routes
 const exampleRoutes = require('./routes/exampleRoutes');
 app.use('/api/example', exampleRoutes);
 
@@ -25,7 +28,4 @@ app.use('/api/expense', expenseRoutes);
 const categoryRoutes = require('./routes/category');
 app.use('/api/category', categoryRoutes);
 
-// Start the server
-app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
-});
+module.exports = app;
